9aa77d4a0590bd1ad7491ea32ecc437bc561d57b
VIGNESH.M <vignesh.m@ramrajcotton.net>
2023-11-10 17:04:07 +0530

    package change

--- lib/app/config.dart
+++ lib/app/config.dart
@@ -6,16 +6,23 @@ class EndPoints {
   static const String auth = 'auth';
   static const String getUsers = 'user/all';
   static const String postUsers = 'user/add';
+  static const String putUsers = 'user/update';
   static const String getContacts = 'contacts/all';
   static const String postContacts = 'contacts/add';
+  static const String putContacts = 'contacts/update';
   static const String getProducts = 'products/all';
   static const String postProducts = 'products/add';
+  static const String putProducts = 'products/update';
   static const String getCompanies = 'companies/all';
   static const String postCompanies = 'companies/add';
+  static const String putCompanies = 'companies/update';
   static const String getCoa = 'coa/all';
   static const String postCoa = 'coa/add';
+  static const String putCoa = 'coa/update';
   static const String getFYear = 'fYear/all';
   static const String postFYear = 'fYear/add';
+  static const String putFYear = 'fYear/update';
   static const String getTransHeader = 'transHeader/all';
   static const String postTransaction = 'transaction/add';
+  static const String putTransaction = 'transaction/update';
 }
